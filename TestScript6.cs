7dc9d1628e7a7437d08459667a73046c28d973f3
korenosuke <takimotoyasuyuki@takimotoyasuyuki-no-MacBook-Air.local>
2016-06-01 22:26:47 +0900

    名前かわったかテスト

---
+++ TestScript6.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+//クラス名とファイル名は同じ
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//オブジェクトの移動(x軸,y軸,z軸)
+		transform.Translate(0, 0, 0);
+	}
+}
